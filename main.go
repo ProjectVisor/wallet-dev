58890f2ce696c65e02dfd8ee47219a7bba102592
Samuel Robinson <samuel.robinson@tangazzoo.com>
2018-03-25 14:05:23 +0100

    base

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"flag"
+	"time"
 
+	"encoding/json"
+
+	"github.com/asticode/go-astilectron"
+	"github.com/asticode/go-astilectron-bootstrap"
+	"github.com/asticode/go-astilog"
 	"github.com/pkg/errors"
 )
+
+// Constants
+const htmlAbout = `Welcome on <b>Astilectron</b> demo!<br>
+This is using the bootstrap and the bundler.`
 
 // Vars
 var (
@@ -22,6 +32,7 @@ func main() {
 	// Run bootstrap
 	astilog.Debugf("Running app built at %s", BuiltAt)
 	if err := bootstrap.Run(bootstrap.Options{
+		Asset: Asset,
 		AstilectronOptions: astilectron.Options{
 			AppName:            AppName,
 			AppIconDarwinPath:  "resources/icon.icns",
@@ -32,14 +43,38 @@ func main() {
 		MenuOptions: []*astilectron.MenuItemOptions{{
 			Label: astilectron.PtrStr("File"),
 			SubMenu: []*astilectron.MenuItemOptions{
-				{Label: astilectron.PtrStr("About")},
+				{
+					Label: astilectron.PtrStr("About"),
+					OnClick: func(e astilectron.Event) (deleteListener bool) {
+						if err := bootstrap.SendMessage(w, "about", htmlAbout, func(m *bootstrap.MessageIn) {
+							// Unmarshal payload
+							var s string
+							if err := json.Unmarshal(m.Payload, &s); err != nil {
+								astilog.Error(errors.Wrap(err, "unmarshaling payload failed"))
+								return
+							}
+							astilog.Infof("About modal has been displayed and payload is %s!", s)
+						}); err != nil {
+							astilog.Error(errors.Wrap(err, "sending about event failed"))
+						}
+						return
+					},
+				},
 				{Role: astilectron.MenuItemRoleClose},
 			},
 		}},
 		OnWait: func(_ *astilectron.Astilectron, iw *astilectron.Window, _ *astilectron.Menu, _ *astilectron.Tray, _ *astilectron.Menu) error {
 			w = iw
+			go func() {
+				time.Sleep(5 * time.Second)
+				if err := bootstrap.SendMessage(w, "check.out.menu", "Don't forget to check out the menu!"); err != nil {
+					astilog.Error(errors.Wrap(err, "sending check.out.menu event failed"))
+				}
+			}()
 			return nil
 		},
+		MessageHandler: handleMessages,
+		RestoreAssets:  RestoreAssets,
 		WindowOptions: &astilectron.WindowOptions{
 			BackgroundColor: astilectron.PtrStr("#333"),
 			Center:          astilectron.PtrBool(true),
